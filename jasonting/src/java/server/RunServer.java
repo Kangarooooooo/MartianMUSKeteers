a17a73242a70f3edb67495f6e43dc2a40274eefe
Martin <Martin@82.211.202.149>
2017-05-03 11:19:48 +0200

    Runserver

---
+++ jasonting/src/java/server/RunServer.java
@@ -0,0 +1,15 @@
+package server;
+
+import massim.Server;
+
+public class RunServer {
+
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+			public void run() {
+			Server.main(new String[] { "-conf", "conf/SampleConfig.json", "--monitor" });
+			}
+			}).start();
+	}
+
+}
